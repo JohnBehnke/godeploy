4792b4ddb90914f5ff28e8d4dadbf4943c8573e5
John Behnke <behnkej@me.com>
2017-08-20 00:35:26 -0400

    cmd line arguments and progess bars

--- deploy.go
+++ deploy.go
@@ -2,19 +2,16 @@
 package main
 
 import (
-	// "flag"
+	"flag"
 	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"gopkg.in/cheggaaa/pb.v1"
 	"io"
 	"io/ioutil"
 	"os"
 )
-
-// func isDirectory(path string) bool {
-
-// }
 
 func isDir(path string) bool {
 	file, err := os.Stat(path)
@@ -24,8 +21,27 @@ func isDir(path string) bool {
 	}
 	return file.Mode().IsDir()
 }
+func getFileCount(path string) int {
+	count := 0
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		fmt.Println(err)
+	}
+	for _, d := range files {
+		var pathToCheck = path + d.Name()
+		if d.Name() != ".git" && d.Name() != ".DS_Store" {
+			if isDir(pathToCheck) {
+				count = count + getFileCount(pathToCheck+"/")
+			} else {
+				count = count + 1
+			}
+		}
+	}
+	return count
 
-func uploadDirectory(session *session.Session, bucketPrefix string, dirPath string) {
+}
+
+func uploadDirectory(session *session.Session, progessBar *pb.ProgressBar, bucketPrefix string, dirPath string) {
 
 	files, err := ioutil.ReadDir(dirPath)
 	if err != nil {
@@ -35,24 +51,17 @@ func uploadDirectory(session *session.Session, bucketPrefix string, dirPath stri
 	for _, d := range files {
 		var pathToCheck = dirPath + d.Name()
 		if d.Name() != ".git" && d.Name() != ".DS_Store" {
-
 			if isDir(pathToCheck) {
-				// fmt.Println(strings.Repeat("\t", num), d.Name())
-				// printDir(pathToCheck, num+1)
-				uploadDirectory(session, d.Name()+"/", pathToCheck+"/")
+				uploadDirectory(session, progessBar, d.Name()+"/", pathToCheck+"/")
 			} else {
-				// fmt.Println(strings.Repeat("\t", num), d.Name()
-				uploadFile(session, "testjlb", bucketPrefix, dirPath, d.Name())
+				uploadFile(session, progessBar, "testjlb", bucketPrefix, dirPath, d.Name())
 			}
 		}
-
 	}
-
 }
 
-func uploadFile(session *session.Session, bucket string, bucketPrefix string, filePath string, fileName string) {
+func uploadFile(session *session.Session, progressBar *pb.ProgressBar, bucket string, bucketPrefix string, filePath string, fileName string) {
 	serviceClient := s3.New(session)
-	fmt.Println(filePath + fileName)
 	file, err := os.Open(filePath + fileName)
 	if err != nil {
 		fmt.Println(err)
@@ -65,11 +74,12 @@ func uploadFile(session *session.Session, bucket string, bucketPrefix string, fi
 		Key:    aws.String(bucketPrefix + fileName),
 		Body:   file,
 	})
-	fmt.Print(file)
 	if err != nil {
 		fmt.Fprint(os.Stderr, "Error %v\n", err)
 		os.Exit(1)
 	}
+	// fmt.Println(t)
+	progressBar.Increment()
 }
 
 func main() {
@@ -80,89 +90,6 @@ func main() {
 	const installScriptPath string = "/Users/john/Documents/Code/Personal/EasyDevTools"
 	const personalSitePath string = "/Users/john/Documents/Code/Personal/johnbehnke.github.io/"
 
-	if _, err := os.Stat(personalSitePath + "assets/files/" + resumeName); !os.IsNotExist(err) {
-		fmt.Println("It is there!")
-		os.Remove(personalSitePath + "/assets/files/" + resumeName)
-	}
-
-	files, err := ioutil.ReadDir(resumePath)
-	if err != nil {
-		fmt.Println(err)
-	}
-	var resume = resumePath + (files[len(files)-1]).Name()
-
-	sourcePDF, err := os.Open(resume + "/" + resumeName)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	defer sourcePDF.Close()
-
-	destinationPDF, err := os.Create(personalSitePath + "/assets/files/" + resumeName)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	defer destinationPDF.Close()
-
-	_, err = io.Copy(destinationPDF, sourcePDF)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	err = destinationPDF.Sync()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	if _, err := os.Stat(personalSitePath + scriptName); !os.IsNotExist(err) {
-		fmt.Println("It is there!")
-		os.Remove(personalSitePath + scriptName + resumeName)
-	}
-
-	sourceScript, err := os.Open(installScriptPath + "/" + scriptName)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	defer sourceScript.Close()
-
-	destinationScript, err := os.Create(personalSitePath + scriptName)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	defer destinationScript.Close()
-
-	_, err = io.Copy(destinationScript, sourceScript)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	err = destinationScript.Sync()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	// siteFiles, err := ioutil.ReadDir(personalSitePath)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-
-	// printDir(personalSitePath, 0)
-
-	// var bucket string
-	// var key string
-
-	sess, err := session.NewSession(&aws.Config{Region: aws.String("us-east-1")})
-	if err != nil {
-		fmt.Print(err)
-	}
-
-	uploadDirectory(sess, "", personalSitePath)
-
 	// uploadFile(sess, "testjlb", "/", filePath, fileName)
 	// serviceClient := s3.New(sess)
 
@@ -171,13 +98,103 @@ func main() {
 	// flag.Parse()
 
 	// t, err := serviceClient.PutObject(&s3.PutObjectInput{
-	// 	Bucket: aws.String(bucket),
-	// 	Key:    aws.String(key),
-	// 	Body:   os.Stdin,
+	//  Bucket: aws.String(bucket),
+	//  Key:    aws.String(key),
+	//  Body:   os.Stdin,
 	// })
 	// fmt.Print(t)
 	// if err != nil {
-	// 	fmt.Fprint(os.Stderr, "Error %v\n", err)
-	// 	os.Exit(1)
+	//  fmt.Fprint(os.Stderr, "Error %v\n", err)
+	//  os.Exit(1)
 	// }
+
+	shouldCopy := flag.Bool("c", false, "Copy new files to site dir")
+	shouldUpload := flag.Bool("u", false, "Upload site to AWS Bucket")
+	flag.Parse()
+
+	if *shouldCopy {
+		fmt.Println("Copying over files...")
+		progressBar := pb.StartNew(2)
+		progressBar.Format("<=👉 >")
+		if _, err := os.Stat(personalSitePath + "assets/files/" + resumeName); !os.IsNotExist(err) {
+			os.Remove(personalSitePath + "/assets/files/" + resumeName)
+		}
+
+		files, err := ioutil.ReadDir(resumePath)
+		if err != nil {
+			fmt.Println(err)
+		}
+		var resume = resumePath + (files[len(files)-1]).Name()
+
+		sourcePDF, err := os.Open(resume + "/" + resumeName)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		defer sourcePDF.Close()
+
+		destinationPDF, err := os.Create(personalSitePath + "/assets/files/" + resumeName)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		defer destinationPDF.Close()
+
+		_, err = io.Copy(destinationPDF, sourcePDF)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		err = destinationPDF.Sync()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		progressBar.Increment()
+
+		if _, err := os.Stat(personalSitePath + scriptName); !os.IsNotExist(err) {
+			os.Remove(personalSitePath + scriptName + resumeName)
+		}
+
+		sourceScript, err := os.Open(installScriptPath + "/" + scriptName)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		defer sourceScript.Close()
+
+		destinationScript, err := os.Create(personalSitePath + scriptName)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		defer destinationScript.Close()
+
+		_, err = io.Copy(destinationScript, sourceScript)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		err = destinationScript.Sync()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		progressBar.Increment()
+		progressBar.FinishPrint("Files successfully copied over 🍻")
+	}
+	if *shouldUpload {
+
+		sess, err := session.NewSession(&aws.Config{Region: aws.String("us-east-1")})
+		if err != nil {
+			fmt.Print(err)
+		}
+		fmt.Println("Uploading Files")
+		progressBar := pb.StartNew(getFileCount(personalSitePath))
+		progressBar.Format("<=👉 >")
+
+		uploadDirectory(sess, progressBar, "", personalSitePath)
+		progressBar.FinishPrint("All Files uploaded! 🍻")
+
+	}
 }
